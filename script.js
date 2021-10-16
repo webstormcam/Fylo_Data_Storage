7a517ed2cb7833a1eef32c5b993c6918d0b56131
Cameron Chandler <webstormcam@gmail.com>
2021-10-15 20:58:44 -0400

    Figured out something
    Put the percentage in JS so that the bars are always equal when loading the bar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bigger(){
-    document.getElementById("used").style.width = "10rem";
+    document.getElementById("used").style.width = "75%";
     document.getElementById("used").style.transition ='all 2s'
 }
 
